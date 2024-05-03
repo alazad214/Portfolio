ca7934ffc882b39fa953bd924251e932f75d27c0
Al Azad <alazad214@gmail.com>
2024-05-04 01:07:27 +0600

    user message page added

--- lib/constants/nav_item.dart
+++ lib/constants/nav_item.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:my_portfilio/pages/blogs.dart';
 import 'package:my_portfilio/pages/homepage.dart';
 import 'package:my_portfilio/pages/settings_screen.dart';
-import 'package:my_portfilio/pages/user_message.dart';
+import 'package:my_portfilio/user_message/user_message.dart';
 
 List<String> nav_titles = ["Home", "Blog", "Message>", "Settings"];
 List<IconData> nav_icons = [
@@ -12,4 +12,9 @@ List<IconData> nav_icons = [
   Icons.message_outlined,
   Icons.settings
 ];
-List nav_pages = [HomePage(), Blogs_page(), User_Message(), Settings_Screen()];
+List nav_pages = [
+  const HomePage(),
+  const Blogs_page(),
+  User_Message(),
+  const Settings_Screen()
+];
